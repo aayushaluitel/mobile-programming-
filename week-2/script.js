629b3d2abd02940bd91399b76da928daf7ae4d0d
unknown <aayushaluitel>
2026-09-08 08:32:22 +0545

    MY JavaScript

---
+++ week-2/script.js
@@ -0,0 +1,13 @@
+function sub() {
+    let number1 = Number(document.getElementById("number1").value);
+    let number2= Number(document.getElementById("number2").value);
+    let sub = number1 -number2;
+    document.getElementById("result").innerHTML= sub;
+}
+
+function add() {
+    let number1 = Number(document.getElementById("number1").value);
+    let number2= Number(document.getElementById("number2").value);
+    let add = number1 + number2;
+    document.getElementById("result").innerHTML= add;
+}
